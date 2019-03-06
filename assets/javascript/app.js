4a0ab3b1fc5b4843f5e4d8629d2a71d0cce47f21
MattSauermilch <madmattschool@gmail.com>
2019-03-06 12:36:32 -0600

    fixed userClick and shifting questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
-
+    //questions, objects in an array
     var questionArray = [
         {   question : "What year did Harley Davidson invite the V-twin engine?",
                 ans : "1909",
@@ -213,120 +213,122 @@ $(document).ready(function(){
             w3 : "w3",
             },  
     ];
-
-    //creates and array that counts the number on objects in question array
-    var numArray = [];
-
-        for (i=0; i<questionArray.length; i++){
-            numArray.push(i);
-        };
     
-    //randomizes the order of numbers in numArray 
+    //creates an array that counts the number on objects in question array
+    var numArray = [];
+        
+    //populates the array with the length of questionArray
+    for (i=0; i<questionArray.length; i++){
+        numArray.push(i);
+    };
+
+    //randomizes the order of numbers in numArray to be used to randomly pull questions and corropnding answers
     function shuffleNumbers(){
-        for (var i = numArray.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = numArray[i];
-            numArray[i] = numArray[j];
-            numArray[j] = temp;
+        for (var j = numArray.length - 1; j > 0; j--) {
+            var k = Math.floor(Math.random() * (j + 1));
+            var temp = numArray[j];
+            numArray[j] = numArray[k];
+            numArray[k] = temp;
         };
     };
-        shuffleNumbers();
-            console.log("shuffled numbers: " + numArray);
 
-    var i = numArray[0]; //current question Object's location reference number
+    shuffleNumbers();
+
+    //rounderCounter counts down numArray to end game when empty
     var roundCounter = 0;
     var score = 0;
-    
+
 
 //---------------------------------------------------------------------------
     //Game code
-    function play() {
-            //question, answer, and wrong answer from object array
-            var question = questionArray[i].question;
+    function play(answer) {
+            //pulls first number from numArray to Q&A
+            var i = numArray.shift();
+                   
             var answer = questionArray[i].ans;
             var wrongAns1 = questionArray[i].w1;
             var wrongAns2 = questionArray[i].w2;
             var wrongAns3 = questionArray[i].w3;
 
-                // array of answer options
-                var ansArray = [answer, wrongAns1, wrongAns2, wrongAns3];
+            // array of answer options
+            var ansArray = [answer, wrongAns1, wrongAns2, wrongAns3];
                 
-                    //Shuffles answer choices, return shuffled ansArray;
-                    function shuffleQuestions() {
-                        for (var i = ansArray.length - 1; i > 0; i--) {
-                        var j = Math.floor(Math.random() * (i + 1));
-                        var temp = ansArray[i];
-                        ansArray[i] = ansArray[j];
-                        ansArray[j] = temp;
+                //Shuffles answer choices, return shuffled ansArray;
+                function shuffleQuestions() {
+                    for (var l = ansArray.length - 1; l > 0; l--) {
+                    var m = Math.floor(Math.random() * (l + 1));
+                    var temp = ansArray[l];
+                    ansArray[l] = ansArray[m];
+                    ansArray[m] = temp;
                         };
                     };
 
-                    shuffleQuestions();
+                shuffleQuestions();
+                    console.log(ansArray);
 
         function questAnsConsole(){
 
                //Writes Question on screen.
                 $(".questionBox").text(questionArray[i].question);
+                    console.log(questionArray[i].question)
                     //Writes Answer on screen.
-                    $(".answerBox1").text(ansArray[0]).attr("value", ansArray[0]);
-                    $(".answerBox2").text(ansArray[1]).attr("value", ansArray[1]);
-                    $(".answerBox3").text(ansArray[2]).attr("value", ansArray[2]);
-                    $(".answerBox4").text(ansArray[3]).attr("value", ansArray[3]);
+                    $(".answerBox1").text(ansArray[0]).attr("data-item", ansArray[0]);
+                    $(".answerBox2").text(ansArray[1]).attr("data-item", ansArray[1]);
+                    $(".answerBox3").text(ansArray[2]).attr("data-item", ansArray[2]);
+                    $(".answerBox4").text(ansArray[3]).attr("data-item", ansArray[3]);
+                };
 
-                
-          };
-          questAnsConsole();
-          setClickEvent();
-                                    //timer
-                                    // setInterval(timer, 1000);
-                                    // var clock = 30;
-
-                                    // function timer(){
-                                    //     clock--;
-                                    //     $(".timer").text(clock);
-                                    // console.log(clock);
-                                    // };
-
+        questAnsConsole(); 
       };// closes Play
+        
+      console.log(answer);
 
+      setClickEvent();
+                              
     play();
 
         function setClickEvent(){
 
             $("button").click(function() {
-               var userClick = document.getElementById(".button").Value;
-               console.log(this);
-               alert(userClick);
+               var userClick = $(this).attr("data-item");
+               console.log("this: " + this);
+               console.log(userClick);
 
-            if (roundCounter === numArray.length){                                
+            if ( numArray.length === false){                                
                 $(".questionBox").text("You got " + score + " out of " + numArray.length + "!"); //Format text so that it is larger!
                 $(".answerArea").hide();
+            };
 
-            } else if (questionArray[i].ans === userClick) {
+            if (questionArray[i].ans === userClick) {
+                console.log("answer: " + answer);
+                console.log(userClick);
                 alert("winner winner chicken dinner!");
                 score++;
-                roundCounter++;
+                // roundCounter++;
+                
                 play();
 
             } else {
-                alert("yea lost!");
-               roundCounter++;
+                // alert("yea lost!");
+                // roundCounter++;
+                
                 play();
             };
-            console.log("number of rounds " + roundCounter);
-            console.log(score);
+
+            // console.log("number of rounds " + roundCounter); 
+            console.log("User Score: " + score);
            });
         };
 }); // closes .ready
 
-
+// click event cant not reference "answer"
 
 //reset
 
 // add timer with numbers on index page
 
 
-// format index
+// format index page
 
-//<button data-questionNum=24 > </button>
+
 
